0fb37658e5069a71386bd57ff6a02f94fc839e20
arpicode <arpicode@gmail.com>
2023-04-11 08:21:41 +0200

    Add favicon

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require('path')
 const express = require('express')
 const logger = require('morgan')
+const favicon = require('serve-favicon')
 
 const apiRouter = require('./api/routes/index')
 
@@ -7,8 +9,9 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(logger('dev'))
-app.use(express.static('public'))
+app.use(express.static(path.join('public')))
 app.use(express.json())
 app.use('/api', apiRouter)
+app.use(favicon(path.join('public', 'img', 'favicon.png')))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
